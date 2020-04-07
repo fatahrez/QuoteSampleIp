ba7d078a36e557c9f24cd122046de9ebb649e767
fatahrez <fertahrez@gmail.com>
2020-04-07 13:24:29 +0300

    Add model class quote.ts for adding new Quote object and to be used as blueprint for the app

---
+++ src/app/quote.ts
@@ -0,0 +1,9 @@
+export class Quote {
+  public showQuote: boolean;
+
+  constructor(public author: string, public submitter: string, public theQuote: string, public upVote: number, public downVote: number, public date: Date) {
+    this.showQuote = false;
+    this.upVote = 0;
+    this.downVote = 0;
+  }
+}
