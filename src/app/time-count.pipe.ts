5bc8179e9b5b6d574a6060c7cb0ce64cd941d437
fatahrez <fertahrez@gmail.com>
2020-04-07 18:47:08 +0300

    add pipe to count time past since quote was posted

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -1,12 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'timeCount'
+  name: 'timeCount',
+  pure: true
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
-  }
+  transform(value: any, args?: any): any {
+    const today: Date = new Date();
+    const todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const dateDifference = Math.abs(value - todayWithNoTime);
+    const secondsInDay = 86400;
+    const dateDifferenceSeconds = dateDifference * 0.001;
+    const dateCounter = dateDifferenceSeconds / secondsInDay;
 
+    if (dateCounter >= 1 && value > todayWithNoTime){
+      return dateCounter;
+    } else {
+      return 0;
+    }
+  }
 }
