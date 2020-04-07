54289d191bd5bdb98425596247385e64f4bc52aa
fatahrez <fertahrez@gmail.com>
2020-04-07 13:37:08 +0300

    Add quotes array in quote.component.ts to be the first quotes to be displayed

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+import DateTimeFormat = Intl.DateTimeFormat;
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote('Martin Luther King Jr.', 'Abdulfatah', 'If you cant fly then run, if you cant run then walk, if you cant walk then crawl, but whatever you do, you have to keep moving forward.', 0, 0, new Date(2020, 4, 6)),
+    new Quote('Oprah Winfrey', 'Abdulfatah', 'Where there is no struggle, there is no strength.', 0, 0, new Date(2020, 4, 6))
+  ];
+
+
 
   constructor() { }
 
